9624efe6e3e0cd3b044825a86ca12e8d36f782e1
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-10-26 14:15:43 +0200

    Add partial implementation of POST /documents

--- server/src/model/document.ts
+++ server/src/model/document.ts
@@ -1,1 +1,16 @@
-export class Document {}
+export class Document {
+  id: number;
+  title: string;
+  description: string;
+
+  constructor(id: number, title: string, description: string) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+  }
+
+  static async create(title: string, description: string): Promise<Document> {
+    const id = 3; //TODO: get from insertion in the DB
+    return new Document(id, title, description);
+  }
+}
