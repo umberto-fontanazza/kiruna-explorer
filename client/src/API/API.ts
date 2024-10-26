1e1fa1560240a1c8a57b05cc0fc10433c56de085
PAOLO MICHELOTTI s328846 <s328846@studenti.polito.it>
2024-10-26 11:16:07 +0200

    API calls new

--- client/src/API/API.ts
+++ client/src/API/API.ts
@@ -28,9 +28,129 @@ const logout = async () => {
   }
 };
 
+/*************************   DOCUMENTS   *****************************/
+
+async function getDocuments() {
+  try{
+    const response = await fetch(baseURL + "/documents");
+    if(response.ok){
+      const documents = await response.json();
+      return documents;
+    }
+  }catch(err){
+    console.error(err);
+  }
+}
+
+async function postDocument(id: number, title: string, description: string) {
+  try{
+    const response = await fetch(baseURL + `/documents/${id}`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({title, description}),
+    });
+    if(response.ok){
+      return true;
+    }
+  }catch(err){
+    console.error(err);
+  }
+}
+
+async function patchDocument(id: number, title: string, coordinates: string) {  // other fields?
+  try{
+    const response = await fetch(baseURL + `/documents/${id}`, {
+      method: "PATCH",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({title, coordinates}),
+    });
+    if(response.ok){
+      return true;
+    }
+  }catch(err){
+    console.error(err);
+  }
+}
+
+async function deleteDocument(id: number) {
+  try{
+    const response = await fetch(baseURL + `/documents/${id}`, {
+      method: "DELETE",
+      credentials: "include",
+    });
+    if(response.ok){
+      return true;
+    }
+  }catch(err){
+    console.error(err);
+  }
+}
+
+
+/****************************   LINKS   **********************************/
+
+
+async function getLinks(id: number) {
+  try{
+    const response = await fetch(baseURL + `/links/${id}`);
+    if(response.ok){
+      const links = await response.json();
+      return links;
+    }
+  }catch(err){
+    console.error(err);
+  }
+}
+
+async function putLink(sourceNodeId: number, targetNodeId: number, linkType: string) {
+  try{
+    const response = await fetch(baseURL + `/links`, {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({sourceNodeId, targetNodeId, linkType}),
+    });
+    if(response.ok){
+      return true;
+    }
+  }catch(err){
+    console.error(err);
+  }
+}
+
+async function deleteLink(sourceNodeId: number, targetNodeId: number, linkType: string) {
+  try{
+    const response = await fetch(baseURL + `/links`, {
+      method: "DELETE",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({sourceNodeId, targetNodeId, linkType}),
+    });
+    if(response.ok){
+      return true;
+    }
+  }catch(err){
+    console.error(err);
+  }
+}
+
+
+
 const API = {
   login,
   logout,
+  getDocuments, postDocument, patchDocument, deleteDocument,
+  getLinks, putLink, deleteLink,
 };
 
 export default API;
