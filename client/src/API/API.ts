39b5364c573a0ad7edf889a90f107e09be46e8a6
PAOLO MICHELOTTI s328846 <s328846@studenti.polito.it>
2024-10-27 17:43:29 +0100

    Sidebar movement and first idea

--- client/src/API/API.ts
+++ client/src/API/API.ts
@@ -44,15 +44,24 @@ async function getDocuments() {
   // }
 
   return [
-    {id: 1, title: "Document 1", description: "Description 1"},
-    {id: 2, title: "Document 2", description: "Description 2"},
-    {id: 3, title: "Document 3", description: null},
-    {id: 4, title: "Document 4", description: null},
-    {id: 5, title: "Document 5", description: "Description 5"},
-    {id: 6, title: "Document 6", description: "Description 6"},
-    {id: 7, title: "Document 7", description: "Description 7"},
-    {id: 8, title: "Document 8", description: "Description 8"},
-    {id: 9, title: "Document 9", description: "Description 9"},
+    {id: 1, title: "Compilation of responses “So what the people of Kiruna think?” (15)",
+      description: "This document is a compilation of the responses to the survey 'What is your impression of Kiruna?' From the citizens' responses to this last part of the survey, it is evident that certain buildings, such as the Kiruna Church, the Hjalmar Lundbohmsgården, and the Town Hall, are considered of significant value to the population. The municipality views the experience of this survey positively, to the extent that over the years it will propose various consultation opportunities."},
+    {id: 2, title: "Detail plan for Bolagsomradet Gruvstadspark (18)", 
+      description: " This is the first of 8 detailed plans located in the old center of Kiruna, aimed at transforming the residential areas into mining industry zones to allow the demolition of buildings. The area includes the town hall, the Ullspiran district, and the A10 highway, and it will be the first to be dismantled. The plan consists, like all detailed plans, of two documents: the area map that regulates it, and a text explaining the reasons that led to the drafting of the plan with these characteristics. The plan gained legal validity in 2012."},
+    {id: 3, title: "Development Plan (41)", 
+      description: "The development plan shapes the form of the new city. The document, unlike previous competition documents, is written entirely in Swedish, which reflects the target audience: the citizens of Kiruna. The plan obviously contains many elements of the winning masterplan from the competition, some recommended by the jury, and others that were deemed appropriate to integrate later. The document is divided into four parts, with the third part, spanning 80 pages, describing the shape the new city will take and the strategies to be implemented for its relocation through plans, sections, images, diagrams, and texts. The document also includes numerous studies aimed at demonstrating the future success of the project."},
+    {id: 4, title: "Deformation forecast (45)", 
+      description: "The development plan shapes the form of the new city. The document, unlike previous competition documents, is written entirely in Swedish, which reflects the target audience: the citizens of Kiruna. The plan obviously contains many elements of the winning masterplan from the competition, some recommended by the jury, and others that were deemed appropriate to integrate later. The document is divided into four parts, with the third part, spanning 80 pages, describing the shape the new city will take and the strategies to be implemented for its relocation through plans, sections, images, diagrams, and texts. The document also includes numerous studies aimed at demonstrating the future success of the project."},
+    {id: 5, title: "Adjusted development plan (47)", 
+      description: "This document is the update of the Development Plan, one year after its creation, modifications are made to the general master plan, which is published under the name 'Adjusted Development Plan91,' and still represents the version used today after 10 years. Certainly, there are no drastic differences compared to the previous plan, but upon careful comparison, several modified elements stand out. For example, the central square now takes its final shape, as well as the large school complex just north of it, which appears for the first time."},
+    {id: 6, title: "Document 6", 
+      description: null},
+    {id: 7, title: "Document 7", 
+      description: "Description 7"},
+    {id: 8, title: "Document 8", 
+      description: "Description 8"},
+    {id: 9, title: "Document 9", 
+      description: "Description 9"},
   ]
 }
 
