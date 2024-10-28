aa0e95e50e86a4ab1467e6d32b9d21b5f3d14d78
lorericci <s332138@studenti.polito.it>
2024-10-29 00:13:06 +0100

    added navigation Home to Login

--- client/src/components/NavHeader.tsx
+++ client/src/components/NavHeader.tsx
@@ -1,17 +1,26 @@
 import { FC } from "react";
 import "../styles/NavHeader.scss"; // Importiamo il file SCSS personalizzato
+import { useNavigate } from "react-router-dom";
 interface NavHeaderProps {
   logout: () => void;
+  login: boolean;
 }
 
 const NavHeader: FC<NavHeaderProps> = (props): JSX.Element => {
+  const nav = useNavigate();
   return (
     <nav className="nav-header">
       <div className="nav-container">
         <div className="nav-brand">Kiruna eXplorer.</div>
-        <button className="btn-logout" onClick={props.logout}>
-          Logout
-        </button>
+        {props.login ? (
+          <button className="btn-logout" onClick={props.logout}>
+            Logout
+          </button>
+        ) : (
+          <button className="btn-login" onClick={() => nav("/login")}>
+            Login
+          </button>
+        )}
       </div>
     </nav>
   );
