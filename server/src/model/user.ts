e198e5982a8254b69bf0bc9960d859cfc747b22e
emanuelefrisi <s1092866@studenti.univpm.it>
2024-10-28 18:11:18 +0100

    Created User model with and the get method to retrieve a single user from the db

---
+++ server/src/model/user.ts
@@ -0,0 +1,63 @@
+import { strict as assert } from "assert";
+import crypto from "crypto";
+
+type UserDbRow = {
+  email: string;
+  name: string;
+  surname: string;
+  role: UserRole;
+};
+
+export enum UserRole {
+  PLANNER,
+  RESIDENT,
+  DEVELOPER,
+}
+
+export class User {
+  email: string;
+  name: string;
+  surname: string;
+  role: UserRole;
+
+  constructor(email: string, name: string, surname: string, role: UserRole) {
+    this.email = email;
+    this.name = name;
+    this.surname = surname;
+    this.role = role;
+  }
+
+  private static fromDatabaseRow(dbRow: UserDbRow): User {
+    const { email, name, surname, role } = dbRow;
+    assert(typeof email === "string");
+    assert(typeof name === "string");
+    assert(typeof surname === "string");
+    assert(typeof role === "string");
+    return new User(email, name, surname, role);
+  }
+
+  static async get(email: string, password: string): Promise<User | false> {
+    const result = await Database.query("SELECT * FROM user WHERE email = $1", [
+      email,
+    ]);
+    const userRow = result.rows[0];
+    const user = User.fromDatabaseRow(userRow);
+
+    return new Promise((resolve, reject) => {
+      crypto.scrypt(password, userRow.salt, 32, (err, hashedPassword) => {
+        if (err) return reject(err);
+
+        if (
+          crypto.timingSafeEqual(
+            Buffer.from(userRow.password, "hex"),
+            hashedPassword,
+          )
+        ) {
+          resolve(user);
+        } else {
+          resolve(false);
+        }
+      });
+    });
+  }
+}
