8224e13b1f96d4385644828823ba713331e727a2
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-10-29 14:58:26 +0100

    Add id request parameter validation

--- server/src/validation/documentSchema.ts
+++ server/src/validation/documentSchema.ts
@@ -1,5 +1,9 @@
 import { z } from "zod";
 
+export const idRequestParam = z.object({
+  id: z.coerce.number().min(1),
+});
+
 export type PostBody = z.infer<typeof postBody>;
 export const postBody = z.object({
   title: z.string().min(1),
