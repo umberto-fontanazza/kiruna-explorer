fc076293aad92a16f4de51b4b892b91e8bd0bc84
emanuelefrisi <s1092866@studenti.univpm.it>
2024-10-29 18:09:26 +0100

    Created UserError class and added to router errors 409 and 500

--- server/src/router/userRouter.ts
+++ server/src/router/userRouter.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { User } from "../model/user";
+import { UserError } from "../error/userError";
 
 export const userRouter: Router = Router();
 
@@ -8,13 +9,28 @@ export const userRouter: Router = Router();
 //  - all fields required
 //  - invalidid user role
 userRouter.post("/", async (req: Request, res: Response) => {
-  const { email, name, surname, role, password } = req.body;
+  try {
+    const { email, name, surname, role, password } = req.body;
 
-  const user = new User(email, name, surname, role);
+    const existingUser = await User.getByEmail(email);
+    if (existingUser) {
+      throw UserError.Conflict(); // Lancia errore 409
+    }
 
-  await User.insert(user, password);
+    const user = new User(email, name, surname, role);
+    await User.insert(user, password);
 
-  res
-    .status(StatusCodes.CREATED)
-    .json({ message: "User created successfully" });
+    res
+      .status(StatusCodes.CREATED)
+      .json({ message: "User created successfully" });
+  } catch (error) {
+    if (error instanceof UserError) {
+      res.status(error.statusCode).json({ message: error.message });
+    } else {
+      console.error(error);
+      res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ message: "An unexpected error occurred on the server" });
+    }
+  }
 });
