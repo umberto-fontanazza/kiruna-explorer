0beab0e22d8501d32cb8046b07c9d94fd35ea72f
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-11-01 22:19:05 +0100

    Add PUT /documents/:id/links schema validation

--- server/src/router/linkRouter.ts
+++ server/src/router/linkRouter.ts
@@ -1,19 +1,27 @@
 import { Request, Response, Router } from "express";
-import { Link, LinkType } from "../model/link";
+import { Link } from "../model/link";
 import { StatusCodes } from "http-status-codes";
+import {
+  validateBody,
+  validateRequestParameters,
+} from "../middleware/validation";
+import { PutBody, putBody } from "../validation/linkSchema";
+import { idRequestParam } from "../validation/documentSchema";
 
 export const linkRouter: Router = Router({ mergeParams: true }); // merge params allows using doc id
 
 linkRouter.put(
   "",
   //TODO: authentication authorization
-  //TODO: validation
+  validateRequestParameters(idRequestParam),
+  validateBody(putBody),
   async (request: Request, response: Response) => {
     const sourceDocumentId = Number(request.params.id);
-    const { targetDocumentId } = request.body;
-    const linkTypes = request.body.linkTypes;
-    const newLink = new Link(sourceDocumentId, targetDocumentId, linkTypes); //TODO:
-    await newLink.update();
+    const body = request.body as PutBody;
+    const { targetDocumentId, linkTypes } = body;
+    const link = new Link(sourceDocumentId, targetDocumentId, linkTypes);
+    await link.update();
     response.status(StatusCodes.CREATED).send();
+    return;
   },
 );
