d73e6dd8c0a41792ea1551ebab32cda2f918bb4f
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-11-01 22:08:51 +0100

    Add PUT /documents/:id/links draft missing validation, auth, transaction

--- server/src/model/link.ts
+++ server/src/model/link.ts
@@ -1,8 +1,11 @@
+import { Database } from "../database";
+import { strict as assert } from "assert";
+
 export enum LinkType {
-  Direct = "direct",
-  Collateral = "collateral",
-  Projection = "projectiobn",
-  Update = "update",
+  Direct = "DIRECT",
+  Collateral = "COLLATERAL",
+  Projection = "PROJECTION",
+  Update = "UPDATE",
 }
 
 export class Link {
@@ -14,5 +17,20 @@ export class Link {
     this.sourceId = sourceId;
     this.targetId = targetId;
     this.types = types;
+  }
+
+  async update(): Promise<void> {
+    //TODO: the two queries should be in a transaction
+    const args = [this.targetId, JSON.stringify(this.types), this.sourceId];
+    let result = await Database.query(
+      "UPDATE document SET links[$1] = $2 where id = $3",
+      args,
+    );
+    assert(result.rowCount === 1);
+    result = await Database.query(
+      "UPDATE document SET links[$3] = $2 where id = $1",
+      args,
+    );
+    assert(result.rowCount === 1);
   }
 }
