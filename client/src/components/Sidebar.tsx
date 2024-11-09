6fd59c97a74d720e56c91f6ea834cd1564a08854
lorericci <s332138@studenti.polito.it>
2024-11-09 16:18:25 +0100

    fixed

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import { FC, useState } from "react";
 import { Document, Link } from "../utils/interfaces";
 import ModalAddConnection from "../components/ModalAddConnection";
+import "../styles/Sidebar.scss";
 
 interface SidebarProps {
   document: Document | null;
@@ -68,7 +69,7 @@ const Sidebar: FC<SidebarProps> = (props) => {
           className="btn-close-sidebar"
           onClick={() => props.setSidebarOpen(false)}
         >
-          <img className="btn-close-img" src="/x.svg" alt="Close" />
+          <img className="btn-close-img" src="x.svg" alt="Close" />
         </button>
       </div>
 
