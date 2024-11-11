6f07508836a376cf6037515d99e39e6dd60b6bbb
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-11-11 17:33:00 +0100

    Move error handler to separate file

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,33 +1,19 @@
 import { NextFunction, Request, Response } from "express";
-import { UserError } from "../error/userError";
 import { StatusCodes } from "http-status-codes";
 import { User, UserRole } from "../model/user";
 
-// Error handling
-export function errorHandling(
-  err: Error,
-  req: Request,
-  res: Response,
+export function isLoggedIn(
+  request: Request,
+  response: Response,
   next: NextFunction,
 ) {
-  if (err instanceof UserError) {
-    res.status(err.statusCode).json({ message: err.message });
-  } else {
-    console.error(err);
-    res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ message: "An unexpected error occurred on the server" });
-  }
-  next();
-}
-
-// Authentication middleware
-export function isLoggedIn(req: Request, res: Response, next: NextFunction) {
-  if (req.isAuthenticated()) {
+  if (request.isAuthenticated()) {
     next();
     return;
   }
-  res.status(StatusCodes.UNAUTHORIZED).json({ error: "Not authenticated" });
+  response
+    .status(StatusCodes.UNAUTHORIZED)
+    .json({ error: "Not authenticated" });
 }
 
 export function isNotLoggedIn(
