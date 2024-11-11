f98a5a87bc486318d40b60593a253966349d1f6c
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-11-11 18:13:06 +0100

    Edit use lambda syntax to return anonymous methods

--- server/src/middleware/validation.ts
+++ server/src/middleware/validation.ts
@@ -5,24 +5,17 @@ import { ParamsDictionary } from "express-serve-static-core";
 
 // Alternative: https://dev.to/osalumense/validating-request-data-in-expressjs-using-zod-a-comprehensive-guide-3a0j#:~:text=import%20%7B%20RequestHandler%20%7D%20from%20%27express%27%3B
 
-const createValidator = (
-  targetExtractor = (request: Request) => request.body,
-) => {
-  return (schema: z.Schema) => {
-    return (
-      request: Request,
-      response: Response,
-      nextFunction: NextFunction,
-    ) => {
-      const { success } = schema.safeParse(targetExtractor(request));
-      if (success) {
-        nextFunction();
-      } else {
-        response.status(StatusCodes.BAD_REQUEST).send();
-      }
-    };
+const createValidator =
+  (targetExtractor = (request: Request) => request.body) =>
+  (schema: z.Schema) =>
+  (request: Request, response: Response, nextFunction: NextFunction) => {
+    const { success } = schema.safeParse(targetExtractor(request));
+    if (success) {
+      nextFunction();
+    } else {
+      response.status(StatusCodes.BAD_REQUEST).send();
+    }
   };
-};
 
 export const validateBody = createValidator((request: Request) => request.body);
 export const validateRequestParameters = createValidator(
