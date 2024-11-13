3a6d5bd0bde1bb5f1e1115b649cdfa4915c7baf0
lorericci <s332138@studenti.polito.it>
2024-11-13 18:36:29 +0100

    flickering fix

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,5 +1,5 @@
 import { GoogleMap, useJsApiLoader, OverlayView } from "@react-google-maps/api";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { Document } from "../utils/interfaces";
 import "@material/web/iconbutton/filled-tonal-icon-button.js";
 import "@material/web/icon/_icon.scss";
@@ -15,11 +15,14 @@ const MapComponent: FC<MapComponentProps> = (props) => {
   // Coordinates for Kiruna, Sweden
   const kirunaCoords = { lat: 67.8558, lng: 20.2253 };
   const [center, setCenter] = useState(kirunaCoords);
+  const [map, setMap] = useState<google.maps.Map | null>(null);
+  const [mapType, setMapType] = useState<string>("satellite");
 
   // Load Google Maps API with API key from environment variables
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+    libraries: ["marker"],
   });
 
   // Styling for the map container
@@ -39,8 +42,9 @@ const MapComponent: FC<MapComponentProps> = (props) => {
   // Map options to control appearance and restrictions
   const mapOptions = isLoaded
     ? {
-        mapTypeId: "satellite",
-        mapTypeControl: true,
+        mapId: "MAP ID",
+        mapTypeId: mapType,
+        mapTypeControl: false,
         mapTypeControlOptions: {
           style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
           mapTypeIds: ["satellite", "roadmap", "hybrid", "terrain"],
@@ -61,53 +65,75 @@ const MapComponent: FC<MapComponentProps> = (props) => {
       }
     : {};
 
+  useEffect(() => {
+    if (isLoaded && map) {
+      const markers: google.maps.marker.AdvancedMarkerElement[] = [];
+
+      props.documents.map((doc) => {
+        if (
+          doc.coordinates.latitude !== null &&
+          doc.coordinates.longitude !== null
+        ) {
+          const markerContent = document.createElement("div");
+          markerContent.className = "map-icon-documents";
+          markerContent.innerHTML = `<img class="doc-img" src="/document-${doc.type}-icon.png" alt="Custom Marker" />`;
+          const marker = new google.maps.marker.AdvancedMarkerElement({
+            map,
+            position: {
+              lat: doc.coordinates.latitude!,
+              lng: doc.coordinates.longitude!,
+            },
+            content: markerContent,
+            title: doc.title,
+          });
+
+          marker.addListener("click", () => {
+            props.setSidebarOpen(true);
+            props.setDocSelected(doc);
+            setCenter({
+              lat: doc.coordinates.latitude!,
+              lng: doc.coordinates.longitude! + 0.0019,
+            });
+          });
+
+          markers.push(marker);
+        }
+      });
+
+      return () => {
+        markers.forEach((marker) => (marker.map = null));
+      };
+    }
+  }, [isLoaded, map, props, props.documents]);
+
   // Render map only when API is loaded
   return isLoaded ? (
     <>
+      <div className="map-types">
+        <select
+          value={mapType}
+          onChange={(e) => setMapType(e.target.value)}
+          required
+        >
+          <option value={"roadmap"}>RoadMap</option>
+          <option value={"satellite"}>Satellite</option>
+          <option value={"hybrid"}>Hybrid</option>
+          <option value={"terrain"}>Terrain</option>
+          {/* <option value="Others">Others</option> */}
+        </select>
+      </div>
       <GoogleMap
         mapContainerStyle={containerStyle}
         zoom={10}
         options={mapOptions}
         center={center}
-      >
-        {/* Render an overlay or marker for each document with valid coordinates */}
-        {props.documents?.map(
-          (doc) =>
-            doc.coordinates.latitude !== null &&
-            doc.coordinates.longitude !== null && (
-              <OverlayView
-                key={doc.id}
-                position={{
-                  lat: doc.coordinates.latitude!,
-                  lng: doc.coordinates.longitude!,
-                }}
-                mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
-              >
-                {/* Custom icon with click handler to open sidebar and recenter map */}
-                <div
-                  className="map-icon-documents"
-                  onClick={() => {
-                    props.setSidebarOpen(true);
-                    props.setDocSelected(doc);
-                    setCenter({
-                      lat: doc.coordinates.latitude!,
-                      lng: doc.coordinates.longitude! + 0.0019,
-                    });
-                  }}
-                >
-                  <img
-                    className="doc-img"
-                    src={`/document-${doc.type}-icon.png`}
-                    alt="Custom Marker"
-                  />
-                </div>
-              </OverlayView>
-            )
-        )}
-      </GoogleMap>
+        onLoad={(mapInstance) => {
+          setMap(mapInstance);
+        }}
+      />
     </>
   ) : (
-    <></>
+    <div>Caricamento...</div>
   );
 };
 
