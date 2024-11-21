a5360e9be96c7132c526248844b82ccc74131c6a
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-11-21 19:15:49 +0100

    Big documentForm refactor

--- client/src/components/DocumentForm.tsx
+++ client/src/components/DocumentForm.tsx
@@ -54,25 +54,6 @@ const DocumentForm: FC<DocumentFormProps> = ({
   const [tableLinks, setTableLinks] = useState<Link[]>([]);
   const [isInitialized, setIsInitialized] = useState<boolean>(false);
 
-  /////// FILE ATTACHMENT CODE ///////
-
-  /*const [files, setFiles] = useState<File[]>([]);
-
-  const handleFileSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedFiles = e.target.files ? Array.from(e.target.files) : [];
-    setFiles(selectedFiles);
-  };
-
-  const handleFileDrop = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    const droppedFiles = Array.from(e.dataTransfer.files);
-    setFiles(droppedFiles);
-  };
-
-  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-  };*/
-
   useEffect(() => {
     if (docSelected) {
       setNewDoc(docSelected);
@@ -136,310 +117,225 @@ const DocumentForm: FC<DocumentFormProps> = ({
   if (!modalOpen || !isInitialized) return null;
   if (page === 1) {
     return (
-      <div className="modal-overlay">
-        <div className="modal-content">
-          <button
-            className="close-button"
-            onClick={() => {
-              setNewDoc(initialDocumentState);
-              onClose();
-            }}
-          >
-            <img src="/x.png" alt="Close" />
-          </button>
-          <h2>{!editDocumentMode ? "Add New Document" : "Update Document"}</h2>
+      <form
+        className="document modal"
+        onSubmit={(ev) => {
+          ev.preventDefault();
+          if (page === 1) {
+            // Passa alla pagina successiva solo se la validazione è passata
+            setPage(2);
+          }
+        }}
+      >
+        <button
+          className="close"
+          onClick={() => {
+            setNewDoc(initialDocumentState);
+            onClose();
+          }}
+        >
+          <img src="/x.png" alt="Close" />
+        </button>
 
-          <ProgressBar currentPage={page} />
+        <h2>{!editDocumentMode ? "Add New Document" : "Update Document"}</h2>
 
-          <form
-            onSubmit={(ev) => {
-              ev.preventDefault();
-              if (page === 1) {
-                // Passa alla pagina successiva solo se la validazione è passata
-                setPage(2);
-              }
-            }}
-          >
-            {/* Title Input */}
-            <div className="form-group">
-              <label htmlFor="title">Title *</label>
-              <input
-                id="title"
-                type="text"
-                placeholder="Enter Document Title"
-                value={newDoc.title}
-                onChange={(e) =>
-                  setNewDoc((prev) => ({ ...prev, title: e.target.value }))
-                }
-                required
-                className="input-title"
-              />
-            </div>
+        <ProgressBar currentPage={page} />
 
-            {/* Description */}
-            <div className="form-group">
-              <label htmlFor="description">Description *</label>
-              <textarea
-                id="description"
-                value={newDoc.description || ""}
-                placeholder="Enter Document Description"
-                onChange={(e) =>
-                  setNewDoc((prev) => ({
-                    ...prev,
-                    description: e.target.value,
-                  }))
-                }
-                required
-              />
-            </div>
-
-            <div className="line">
-              <div className="form-group">
-                <label htmlFor="scale-type">Scale *</label>
-                <select
-                  id="scale-type"
-                  defaultValue={newDoc.scale.type}
-                  onChange={(e) => {
-                    const scaleType = e.target.value;
-                    const scaleRatio =
-                      scaleType === ScaleType.Ratio ? 1 : undefined;
-                    setNewDoc((prev) => ({
-                      ...prev,
-                      scale: {
-                        type: scaleType as ScaleType,
-                        ratio: scaleRatio,
-                      },
-                    }));
-                  }}
-                  required
-                >
-                  {Object.values(ScaleType).map((val) => (
-                    <option key={val} value={val}>
-                      {val}
-                    </option>
-                  ))}
-                </select>
-              </div>
-
-              <div className="form-group">
-                {newDoc.scale.type === ScaleType.Ratio && (
-                  <div className="ratio-group">
-                    <label htmlFor="ratio" className="ratio">
-                      1:{" "}
-                    </label>
-                    <input
-                      id="ratio"
-                      type="number"
-                      min="1"
-                      value={newDoc.scale.ratio}
-                      onChange={(e) =>
-                        setNewDoc((prev) => ({
-                          ...prev,
-                          scale: {
-                            ...prev.scale,
-                            ratio: Number(e.target.value),
-                          },
-                        }))
-                      }
-                      style={{
-                        width: "95%",
-                        marginTop: "7.5%",
-                        padding: "0.7rem",
-                        borderRadius: "8px",
-                        fontSize: "1rem",
-                      }}
-                      required
-                    />
-                  </div>
-                )}
-              </div>
-            </div>
-            <div className="line">
-              {/* Issuance Date */}
-              <div className="form-group">
-                <label htmlFor="issuance-date">Issuance Date</label>
-                <input
-                  id="issuance-date"
-                  type="date"
-                  value={
-                    newDoc.issuanceDate
-                      ? newDoc.issuanceDate.format("YYYY-MM-DD")
-                      : ""
-                  }
-                  onChange={onDateChange}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="document-type">Type *</label>
-                <select
-                  id="document-type"
-                  value={newDoc.type}
-                  onChange={(e) =>
-                    setNewDoc((prev) => ({
-                      ...prev,
-                      type: e.target.value as DocumentType,
-                    }))
-                  }
-                  required
-                >
-                  <option value="">Select type</option>
-                  <option value={DocumentType.Informative}>
-                    Informative Document
-                  </option>
-                  <option value={DocumentType.Prescriptive}>
-                    Prescriptive Document
-                  </option>
-                  <option value={DocumentType.Design}>Design Document</option>
-                  <option value={DocumentType.Technical}>
-                    Technical Document
-                  </option>
-                  <option value={DocumentType.MaterialEffect}>
-                    Material effect
-                  </option>
-                </select>
-              </div>
-            </div>
-
-            <div className="form-group">
-              <label>Stakeholders *</label>
-              <div className="checkbox-group stakeholders">
-                {stakeholdersOptions.map((option) => (
-                  <label key={option.value} className="checkbox-label">
-                    <input
-                      type="checkbox"
-                      value={option.value}
-                      checked={newDoc.stakeholders?.includes(option.value)}
-                      onChange={onCheckboxChange}
-                    />
-                    {option.label}
-                  </label>
-                ))}
-              </div>
-            </div>
-            {/* DO NOT ELIMINATE THIS CODE COMMENTED */}
-            {/* <div className="form-group">
-                  <label>Pages (optional):</label>
-                  <input
-                    type="number"
-                    id="no-spin"
-                    value={newDoc.pages !== null ? newDoc.pages : ""}
-                    onChange={(e) => {
-                      setNewDoc((prev) => ({
-                        ...prev,
-                        pages: e.target.value ? Number(e.target.value) : null,
-                      }));
-                    }}
-                  />
-                </div> */}
-            {/*<div className="form-group">
-                <div
-                  className="file-upload-area"
-                  onDrop={handleFileDrop}
-                  onDragOver={handleDragOver}
-                  style={{
-                    border: "2px dashed #ccc",
-                    padding: "20px",
-                    borderRadius: "5px",
-                    textAlign: "center",
-                    cursor: "pointer",
-                  }}
-                >
-                  <p>Drag and drop files here, or</p>
-                  <label htmlFor="file-upload" className="file-upload-label">
-                    <strong>browse</strong>
-                  </label>
-                  <input
-                    id="file-upload"
-                    type="file"
-                    multiple
-                    accept=".pdf,.doc,.docx,.png,.jpg"
-                    onChange={handleFileSelect}
-                    style={{ display: "none" }}
-                  />
-                </div>
-                
-                {files.length > 0 && (
-                  <div className="uploaded-files-list">
-                    <h4>Files to Upload:</h4>
-                    <ul>
-                      {files.map((file, index) => (
-                        <li key={index}>
-                          {file.name} ({(file.size / 1024).toFixed(2)} KB)
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                )}
-              </div>*/}
-
-            {/* Coordinates */}
-            <div className="line">
-              {/* Latitude */}
-              <div className="form-group">
-                <label>Latitude *</label>
-                <input
-                  type="number"
-                  id="no-spin"
-                  step="0.00000000000001"
-                  name="latitude"
-                  min="-90"
-                  max="90"
-                  value={
-                    newDoc.coordinates?.latitude !== null
-                      ? newDoc.coordinates?.latitude
-                      : ""
-                  }
-                  onChange={(e) =>
-                    setNewDoc((prev: Document) => ({
-                      ...prev,
-                      coordinates: {
-                        latitude: Number(e.target.value),
-                        longitude: prev.coordinates?.longitude ?? 0,
-                      },
-                    }))
-                  }
-                  placeholder="Es. 34.1234"
-                />
-              </div>
-
-              {/* Longitude */}
-              <div className="form-group">
-                <label>Longitude *</label>
-                <input
-                  lang="en"
-                  type="number"
-                  step="0.00000000000001"
-                  id="no-spin"
-                  name="longitude"
-                  min="-180"
-                  max="180"
-                  value={
-                    newDoc.coordinates?.longitude !== null
-                      ? newDoc.coordinates?.longitude
-                      : ""
-                  }
-                  onChange={(e) => {
-                    setNewDoc((prev) => ({
-                      ...prev,
-                      coordinates: {
-                        latitude: prev.coordinates?.latitude ?? 0,
-                        longitude: Number(e.target.value),
-                      },
-                    }));
-                  }}
-                  placeholder="Es. 20.2253"
-                />
-              </div>
-            </div>
-            {/* Form Buttons */}
-            <div className="button-group">
-              <button type="submit" className="submit-button">
-                Continue
-              </button>
-            </div>
-          </form>
+        <div className="form-group title">
+          <label htmlFor="title">Title *</label>
+          <input
+            id="title"
+            type="text"
+            placeholder="Enter Document Title"
+            value={newDoc.title}
+            onChange={(e) =>
+              setNewDoc((prev) => ({ ...prev, title: e.target.value }))
+            }
+            required
+            className="input-title"
+          />
         </div>
-      </div>
+
+        <div className="form-group description">
+          <label htmlFor="description">Description *</label>
+          <textarea
+            id="description"
+            value={newDoc.description || ""}
+            placeholder="Enter Document Description"
+            onChange={(e) =>
+              setNewDoc((prev) => ({
+                ...prev,
+                description: e.target.value,
+              }))
+            }
+            required
+          />
+        </div>
+
+        <div className="form-group scale-type">
+          <label htmlFor="scale-type">Scale *</label>
+          <select
+            id="scale-type"
+            defaultValue={newDoc.scale.type}
+            onChange={(e) => {
+              const scaleType = e.target.value;
+              const scaleRatio = scaleType === ScaleType.Ratio ? 1 : undefined;
+              setNewDoc((prev) => ({
+                ...prev,
+                scale: {
+                  type: scaleType as ScaleType,
+                  ratio: scaleRatio,
+                },
+              }));
+            }}
+            required
+          >
+            {Object.values(ScaleType).map((val) => (
+              <option key={val} value={val}>
+                {val}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div
+          className={`form-group ratio ${newDoc.scale.type === ScaleType.Ratio ? "" : "hidden"}`}
+        >
+          <label htmlFor="ratio" className="ratio">
+            1:{" "}
+          </label>
+          <input
+            id="ratio"
+            type="number"
+            min="1"
+            value={newDoc.scale.ratio}
+            onChange={(e) =>
+              setNewDoc((prev) => ({
+                ...prev,
+                scale: {
+                  ...prev.scale,
+                  ratio: Number(e.target.value),
+                },
+              }))
+            }
+            required
+          />
+        </div>
+
+        <div className="form-group issuance-date">
+          <label htmlFor="issuance-date">Issuance Date</label>
+          <input
+            id="issuance-date"
+            type="date"
+            value={
+              newDoc.issuanceDate
+                ? newDoc.issuanceDate.format("YYYY-MM-DD")
+                : ""
+            }
+            onChange={onDateChange}
+          />
+        </div>
+
+        <div className="form-group document-type">
+          <label htmlFor="document-type">Type *</label>
+          <select
+            id="document-type"
+            value={newDoc.type}
+            onChange={(e) =>
+              setNewDoc((prev) => ({
+                ...prev,
+                type: e.target.value as DocumentType,
+              }))
+            }
+            required
+          >
+            <option value="">Select type</option>
+            <option value={DocumentType.Informative}>
+              Informative Document
+            </option>
+            <option value={DocumentType.Prescriptive}>
+              Prescriptive Document
+            </option>
+            <option value={DocumentType.Design}>Design Document</option>
+            <option value={DocumentType.Technical}>Technical Document</option>
+            <option value={DocumentType.MaterialEffect}>Material effect</option>
+          </select>
+        </div>
+
+        <div className="form-group stakeholders">
+          <label>Stakeholders *</label>
+          <div className="checkbox-group stakeholders">
+            {stakeholdersOptions.map((option) => (
+              <label key={option.value} className="checkbox-label">
+                <input
+                  type="checkbox"
+                  value={option.value}
+                  checked={newDoc.stakeholders?.includes(option.value)}
+                  onChange={onCheckboxChange}
+                />
+                {option.label}
+              </label>
+            ))}
+          </div>
+        </div>
+
+        <div className="form-group latitude">
+          <label>Latitude *</label>
+          <input
+            type="number"
+            id="no-spin"
+            step="0.00000000000001"
+            name="latitude"
+            min="-90"
+            max="90"
+            value={
+              newDoc.coordinates?.latitude !== null
+                ? newDoc.coordinates?.latitude
+                : ""
+            }
+            onChange={(e) =>
+              setNewDoc((prev: Document) => ({
+                ...prev,
+                coordinates: {
+                  latitude: Number(e.target.value),
+                  longitude: prev.coordinates?.longitude ?? 0,
+                },
+              }))
+            }
+            placeholder="Es. 34.1234"
+          />
+        </div>
+
+        <div className="form-group longitude">
+          <label>Longitude *</label>
+          <input
+            lang="en"
+            type="number"
+            step="0.00000000000001"
+            id="no-spin"
+            name="longitude"
+            min="-180"
+            max="180"
+            value={
+              newDoc.coordinates?.longitude !== null
+                ? newDoc.coordinates?.longitude
+                : ""
+            }
+            onChange={(e) => {
+              setNewDoc((prev) => ({
+                ...prev,
+                coordinates: {
+                  latitude: prev.coordinates?.latitude ?? 0,
+                  longitude: Number(e.target.value),
+                },
+              }));
+            }}
+            placeholder="Es. 20.2253"
+          />
+        </div>
+
+        <button type="submit">Continue</button>
+      </form>
     );
   } else if (page === 2) {
     return (
