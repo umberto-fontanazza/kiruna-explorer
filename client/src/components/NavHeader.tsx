e6d8035c05b40f17d9e20e338eee77c29673a1d4
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-11-21 14:50:52 +0100

    Usually the logo brings the user back to the home

--- client/src/components/NavHeader.tsx
+++ client/src/components/NavHeader.tsx
@@ -9,9 +9,10 @@ const NavHeader: FC = (): JSX.Element => {
 
   return (
     <nav className="nav-header">
-      <span className="brand">Kiruna eXplorer.</span>
+      <span className="brand" onClick={() => navigate("/home")}>
+        Kiruna eXplorer.
+      </span>
       <div className={`${user ? "logged-in" : ""}`}>
-        <button onClick={() => navigate("/home")}>Home</button>
         <button onClick={() => navigate("/documents")}>Documents</button>
         {user ? <span className="user-name">Hi {user.name}!</span> : null}
         <button onClick={user ? logout : () => navigate("/login")}>
