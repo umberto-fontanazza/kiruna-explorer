217e550cb7440a354b58dd71c83d634f189a9935
junaidshah2940 <shahjunaid1994@gmail.com>
2024-11-22 16:23:21 +0330

    Added userRouter tests

---
+++ server/test/userRouter.test.ts
@@ -0,0 +1,67 @@
+import dotenv from "dotenv";
+import { StatusCodes } from "http-status-codes";
+import request from "supertest";
+import app from "../src/app";
+dotenv.config();
+
+var counter: any;
+counter = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
+
+describe("POST /users", () => {
+  it("should create a new user when valid data is provided", async () => {
+    const email =
+      (process.env.TEST_EMAIL ?? "defaultemail") + counter + "@gmail.com";
+    const newUser = {
+      email: email,
+      name: process.env.TEST_NAME,
+      surname: process.env.TEST_SURNAME,
+      role: process.env.TEST_ROLE,
+      password: process.env.TEST_PASSWORD,
+    };
+
+    const response = await request(app).post("/users").send(newUser);
+
+    expect(response.status).toBe(StatusCodes.CREATED);
+    expect(response.body.message).toBe("User created successfully");
+  });
+
+  it("should return conflict error if the user already exists", async () => {
+    const email =
+      (process.env.TEST_EMAIL ?? "defaultemail") + counter + "@gmail.com";
+    const newUser = {
+      email: email,
+      name: process.env.TEST_NAME,
+      surname: process.env.TEST_SURNAME,
+      role: process.env.TEST_ROLE,
+      password: process.env.TEST_PASSWORD,
+    };
+
+    const response = await request(app).post("/users").send(newUser);
+
+    expect(response.status).toBe(StatusCodes.CONFLICT);
+    expect(response.body.message).toBe("User already existing");
+  });
+
+  it("should return validation error if the request body is invalid", async () => {
+    const invalidUser = {
+      email: process.env.TEST_INVALID_EMAIL,
+      name: process.env.TEST_NAME,
+    };
+
+    const response = await request(app).post("/users").send(invalidUser);
+
+    expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+    expect(response.body.message).toBeUndefined();
+  });
+
+  it("should return an error if there is an unexpected server issue", async () => {
+    const email =
+      (process.env.TEST_EMAIL ?? "defaultemail") + counter + "@gmail.com";
+    const newUser = {};
+
+    const response = await request(app).post("/users").send(newUser);
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBeUndefined();
+  });
+});
