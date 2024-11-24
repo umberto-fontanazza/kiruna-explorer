2d553252beaf305be505a85349f8a5bf93205b09
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-11-24 22:47:11 +0100

    Add hint

--- server/src/router/uploadRouter.ts
+++ server/src/router/uploadRouter.ts
@@ -3,7 +3,7 @@ import { StatusCodes } from "http-status-codes";
 import { isPlanner } from "../middleware/auth";
 import { validateBody } from "../middleware/validation";
 import { Upload } from "../model/upload";
-import { postBody } from "../validation/uploadSchema";
+import { postBody, PostBody } from "../validation/uploadSchema";
 
 export const uploadRouter: Router = Router();
 
@@ -12,7 +12,8 @@ uploadRouter.post(
   isPlanner,
   validateBody(postBody),
   async (request: Request, response: Response, next: NextFunction) => {
-    const { title, type, documentIds, file: base64file } = request.body;
+    const body = request.body as PostBody;
+    const { title, type, documentIds, file: base64file } = body;
     const file = Buffer.from(base64file, "base64");
     const { id } = await Upload.insert(title, type, file, documentIds);
     response.status(StatusCodes.CREATED).send({ id });
