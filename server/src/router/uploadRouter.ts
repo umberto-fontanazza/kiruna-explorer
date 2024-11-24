7b854ec91c7de726d0679d5a2f858ad3b3596a57
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-11-24 22:45:20 +0100

    Add POST /uploads handler and validation

--- server/src/router/uploadRouter.ts
+++ server/src/router/uploadRouter.ts
@@ -1,12 +1,20 @@
 import { NextFunction, Request, Response, Router } from "express";
+import { StatusCodes } from "http-status-codes";
 import { isPlanner } from "../middleware/auth";
+import { validateBody } from "../middleware/validation";
+import { Upload } from "../model/upload";
+import { postBody } from "../validation/uploadSchema";
 
 export const uploadRouter: Router = Router();
 
 uploadRouter.post(
   "/",
   isPlanner,
+  validateBody(postBody),
   async (request: Request, response: Response, next: NextFunction) => {
-    response.send({ message: "To be implemented" });
+    const { title, type, documentIds, file: base64file } = request.body;
+    const file = Buffer.from(base64file, "base64");
+    const { id } = await Upload.insert(title, type, file, documentIds);
+    response.status(StatusCodes.CREATED).send({ id });
   },
 );
