77516db21ca2bbd2b5021bb283d034d76cc4df3b
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-11-25 13:34:17 +0100

    Format client and add prettier config

--- client/src/components/DocumentForm.tsx
+++ client/src/components/DocumentForm.tsx
@@ -38,7 +38,7 @@ const DocumentForm = () => {
   } = useDocumentFormContext();
   const [page, setPage] = useState<number>(1);
   const [document, setDocument] = useState<DocumentFormType>(
-    documentFormSelected || { ...documentFormDefaults }
+    documentFormSelected || { ...documentFormDefaults },
   );
   const [tableLinks, setTableLinks] = useState<Link[]>(document?.links || []);
 
@@ -64,7 +64,7 @@ const DocumentForm = () => {
           stakeholders: checked
             ? [...(previousDoc.stakeholders ?? []), value]
             : previousDoc.stakeholders?.filter((sh) => sh != value) || [],
-        }) as Document
+        }) as Document,
     );
   };
 
