ffd3194a241a1072988158b0c0cd435a8d008bcb
emanuelefrisi <s1092866@studenti.univpm.it>
2024-11-27 06:03:16 +0100

    Refactor CardDocument structure and css

--- client/src/components/CardDocument.tsx
+++ client/src/components/CardDocument.tsx
@@ -86,96 +86,103 @@ const CardDocument: FC<CardDocumentProps> = (props) => {
 
   return (
     <div className="content">
-      <div className="header">
-        <span
-          className={`material-symbols-outlined color-${fromDocumentTypeToIcon.get(props.document?.type)} size`}
-        >
-          {fromDocumentTypeToIcon.get(props.document?.type)}
-        </span>
-        {props.isDocSelected && (
-          <div className="header-btns">
-            {props.showMapButton && (
-              <button
-                className="btn-map"
-                onClick={() => getDocumentCoordinates()}
-              >
-                <span className="material-symbols-outlined">map</span>
+      <div className="header-section">
+        <div className="header-content">
+          <span
+            className={`material-symbols-outlined color-${fromDocumentTypeToIcon.get(props.document?.type)} size`}
+          >
+            {fromDocumentTypeToIcon.get(props.document?.type)}
+          </span>
+          {props.isDocSelected && (
+            <div className="header-btns">
+              {props.showMapButton && (
+                <button
+                  className="btn-map"
+                  onClick={() => getDocumentCoordinates()}
+                >
+                  <span className="material-symbols-outlined">map</span>
+                </button>
+              )}
+              <button className="btn-download" onClick={handleDownload}>
+                <span className="material-symbols-outlined">file_save</span>
               </button>
-            )}
-            <button className="btn-download" onClick={handleDownload}>
-              <span className="material-symbols-outlined">file_save</span>
-            </button>
-          </div>
-        )}
+            </div>
+          )}
+        </div>
+        <hr />
       </div>
 
-      <hr />
-      <h3>{props.document?.title}</h3>
-      <p>{props.document?.description}</p>
-      <hr />
-      <h4>
-        Stakeholders:&nbsp;
-        {props.document?.stakeholders ? (
-          props.document?.stakeholders?.map((s, index, arr) => (
-            <span key={`${props.document?.id}-${index}`}>
-              {stakeholderDisplay[s]}
-              {index < arr.length - 1 ? ", " : ""}
-            </span>
-          ))
-        ) : (
-          <span>-</span>
-        )}
-      </h4>
-      <h4>
-        Scale:&nbsp;
-        <span>
-          {props.document?.scale.type &&
-            props.document?.scale.type !== ScaleType.Ratio &&
-            scaleTypeDisplay[props.document.scale.type]}
-          {props.document?.scale.type &&
-            props.document?.scale.type === ScaleType.Ratio &&
-            `1:${props.document.scale.ratio}`}
-        </span>
-      </h4>
+      <div className="middle-section">
+        <h3>{props.document?.title}</h3>
+        <p>{props.document?.description}</p>
+      </div>
 
-      <h4>
-        Issuance Date:&nbsp;
-        <span>
-          {props.document?.issuanceDate?.isValid()
-            ? props.document?.issuanceDate?.format("MMMM D, YYYY")
-            : "-"}
-        </span>
-      </h4>
-      <h4>
-        Type:{" "}
-        <span>
-          {props.document?.type && documentTypeDisplay[props.document?.type]}
-        </span>
-      </h4>
-      <div className="connection-group">
+      <div className="bottom-section">
+        <hr />
         <h4>
-          Links:&nbsp;
+          Stakeholders:&nbsp;
+          {props.document?.stakeholders ? (
+            props.document?.stakeholders?.map((s, index, arr) => (
+              <span key={`${props.document?.id}-${index}`}>
+                {stakeholderDisplay[s]}
+                {index < arr.length - 1 ? ", " : ""}
+              </span>
+            ))
+          ) : (
+            <span>-</span>
+          )}
+        </h4>
+        <h4>
+          Scale:&nbsp;
           <span>
-            {props.document?.links?.reduce(
-              (acc: number, link: Link) => acc + link.linkTypes.length,
-              0,
-            ) || 0}
+            {props.document?.scale.type &&
+              props.document?.scale.type !== ScaleType.Ratio &&
+              scaleTypeDisplay[props.document.scale.type]}
+            {props.document?.scale.type &&
+              props.document?.scale.type === ScaleType.Ratio &&
+              `1:${props.document.scale.ratio}`}
           </span>
         </h4>
-        {props.document?.links && props.document?.links.length > 0 && (
-          <button
-            className={`see-links ${visualLinks ? "fill" : "no-fill"}`}
-            onClick={() =>
-              visualLinks ? setVisualLinks(false) : setVisualLinks(true)
-            }
-          >
-            <span
-              className={`material-symbols-outlined dark ${visualLinks ? "fill" : "no-fill"}`}
-            >
-              visibility
+
+        <h4>
+          Issuance Date:&nbsp;
+          <span>
+            {props.document?.issuanceDate?.isValid()
+              ? props.document?.issuanceDate?.format("MMMM D, YYYY")
+              : "-"}
+          </span>
+        </h4>
+        <h4>
+          Type:{" "}
+          <span>
+            {props.document?.type && documentTypeDisplay[props.document?.type]}
+          </span>
+        </h4>
+        <div className="connection-group">
+          <h4>
+            Links:&nbsp;
+            <span>
+              {props.document?.links?.reduce(
+                (acc: number, link: Link) => acc + link.linkTypes.length,
+                0,
+              ) || 0}
             </span>
-          </button>
-        )}
+          </h4>
+          {props.document?.links && props.document?.links.length > 0 && (
+            <button
+              className={`see-links ${visualLinks ? "fill" : "no-fill"}`}
+              onClick={() =>
+                visualLinks ? setVisualLinks(false) : setVisualLinks(true)
+              }
+            >
+              <span
+                className={`material-symbols-outlined dark ${visualLinks ? "fill" : "no-fill"}`}
+              >
+                visibility
+              </span>
+            </button>
+          )}
+        </div>
       </div>
       {user && props.isDocSelected && (
         <div className="btn-group">
