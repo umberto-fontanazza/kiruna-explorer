7691b053b74c10b80668d7c8838b74ff04fe415a
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-11-28 17:51:33 +0100

    Fix polygon insertion

--- server/src/model/polygon.ts
+++ server/src/model/polygon.ts
@@ -27,24 +27,16 @@ export class Polygon {
     client?: PoolClient,
   ): Promise<Polygon> {
     const sqlValues = body
-      .map((_, i) => `ST_Point($${i * 2 - 1}, $${i * 2})::geography`)
+      .map(
+        (_, i) => `ST_Point($${(i + 1) * 2 - 1}, $${(i + 1) * 2})::geography`,
+      )
       .join(", ");
-    const result = await (client ?? Database).query(
-      `INSERT INTO polygon(vertices) VALUES({${sqlValues}}) RETURNING id;`,
-      body.flatMap((c) => [c.longitude, c.latitude]),
-    );
+    const sql = `INSERT INTO polygon(vertices) VALUES(ARRAY[${sqlValues}]) RETURNING id;`;
+    const args = body.flatMap((c) => [c.longitude, c.latitude]);
+    const result = await (client ?? Database).query(sql, args);
     const id: number = result.rows[0].id;
     return new Polygon(id, body);
   }
 
   toResponseBody = (): Coordinates[] => this.vertices;
 }
-
-export const _ = async () => {
-  return await Polygon.insert([
-    { latitude: 67.85584273627312, longitude: 20.22534124638123 },
-    { latitude: 67.85971234872345, longitude: 20.21344576372931 },
-    { latitude: 67.84217863458723, longitude: 20.24528734962184 },
-    { latitude: 67.83997453827193, longitude: 20.23000083719327 },
-  ]);
-};
