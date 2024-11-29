afddb352c6a29de2f9a0ed68cf6f2264b661d1f6
emanuelefrisi <s1092866@studenti.univpm.it>
2024-11-29 01:40:27 +0100

    Big refactor. Second page

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -13,10 +13,12 @@ interface SearchBarProps {
 function SearchBar({ tableLinks, setTableLinks }: SearchBarProps) {
   const { searchableDocuments, setSearchableDocuments } =
     useDocumentFormContext();
-  const [query, setQuery] = useState(""); // user input
+  const [query, setQuery] = useState("");
   const [selectedDocument, setSelectedDocument] = useState<Document>();
-  const [linkType, setLinkType] = useState(LinkType.Direct); // default link type
-  const [filteredDocuments, setFilteredDocuments] = useState<Document[]>([]);
+  const [linkType, setLinkType] = useState(LinkType.Direct);
+  const [filteredSuggestions, setFilteredSuggestions] = useState<Document[]>(
+    [],
+  );
   const [showSuggestions, setShowSuggestions] = useState(false);
 
   useEffect(() => {
@@ -31,7 +33,7 @@ function SearchBar({ tableLinks, setTableLinks }: SearchBarProps) {
     fetchDocuments();
   }, []);
 
-  const handleChange = (e: { target: { value: string } }) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const userInput = e.target.value;
     setQuery(userInput);
 
@@ -42,102 +44,92 @@ function SearchBar({ tableLinks, setTableLinks }: SearchBarProps) {
     setShowSuggestions(true);
   };
 
-  const selectSuggestion = async (suggestion: Document) => {
+  const selectSuggestion = (suggestion: Document) => {
     setQuery(suggestion.title);
-    await setSelectedDocument(suggestion);
+    setSelectedDocument(suggestion);
     setShowSuggestions(false);
   };
 
-  interface HandleAddLinkEvent {
-    preventDefault: () => void;
-  }
-
-  const handleAddLink = (e: HandleAddLinkEvent) => {
+  const handleAddLink = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    if (selectedDocument?.id !== undefined && type !== undefined) {
+    if (selectedDocument?.id && linkType) {
       const targetDocumentId = selectedDocument.id;
-      const target = tableLinks.find(
+      const existingLink = tableLinks.find(
         (link) => link.targetDocumentId === targetDocumentId,
       );
 
-      if (target) {
-        setTableLinks((prev: Link[]) => {
-          const updatedLinks = prev.map((link) => {
-            if (link.targetDocumentId === targetDocumentId) {
-              if (!link.linkTypes.includes(type)) {
-                return {
-                  ...link,
-                  linkTypes: [...link.linkTypes, type],
-                };
-              }
-            }
-            return link;
-          });
-
-          // Return the updated links
-          return updatedLinks;
-        });
+      if (existingLink) {
+        setTableLinks((prev) =>
+          prev.map((link) =>
+            link.targetDocumentId === targetDocumentId &&
+            !link.linkTypes.includes(linkType)
+              ? { ...link, linkTypes: [...link.linkTypes, linkType] }
+              : link,
+          ),
+        );
       } else {
-        setTableLinks((prev: Link[]) => {
-          return [
-            ...prev,
-            {
-              targetDocumentId: targetDocumentId,
-              linkTypes: [type],
-            },
-          ];
-        });
+        setTableLinks((prev) => [
+          ...prev,
+          { targetDocumentId, linkTypes: [linkType] },
+        ]);
       }
     }
   };
 
   return (
     <div className="search-bar">
-      <input
-        type="text"
-        className="search-input"
-        placeholder="Search for a document"
-        value={query}
-        onChange={handleChange}
-        onFocus={() => setShowSuggestions(true)}
-        onBlur={() => setTimeout(() => setShowSuggestions(false), 200)}
-      />
-
-      {showSuggestions && filteredSuggestions.length > 0 && (
-        <div className="form-group">
-          {filteredSuggestions.slice(0, 5).map((suggestion, index) => (
-            <div
-              key={index}
-              onClick={() => selectSuggestion(suggestion)}
-              onKeyDown={(e) => {
-                if (e.key === "Enter" || e.key === " ") {
-                  selectSuggestion(suggestion);
-                }
-              }}
-              className="suggestion-item"
-              onMouseDown={(e) => e.preventDefault()} // Prevent input from losing focus
-              tabIndex={0} // Make the div focusable
-              role="button" // Indicates that the div is acting as a button
-            >
-              {suggestion.title}
-            </div>
-          ))}
+      <div className="search-bar-row">
+        <div className="search-input-container">
+          <img src="magnifying-glass.png" className="search-icon-mg" />
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search for a document"
+            value={query}
+            onChange={handleChange}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
+          />
+          <img
+            src="x-black.png"
+            className="search-icon-x"
+            onClick={() => setQuery("")}
+          />
         </div>
-      )}
-      <select
-        onChange={(e) => {
-          setType(e.target.value as LinkType);
-        }}
-      >
-        {Object.values(LinkType).map((linkType) => (
-          <option key={linkType} value={linkType}>
-            {capitalizeFirstLetter(linkType)}
-          </option>
-        ))}
-      </select>
-      <button className="add-link-button" onClick={(e) => handleAddLink(e)}>
-        Add Link
-      </button>
+        {showSuggestions && filteredSuggestions.length > 0 && (
+          <div className="suggestions-dropdown">
+            {filteredSuggestions.slice(0, 5).map((suggestion) => (
+              <option
+                key={suggestion.id}
+                className="suggestion-item"
+                onMouseDown={() => selectSuggestion(suggestion)}
+                tabIndex={0}
+                role="button"
+              >
+                {suggestion.title}
+              </option>
+            ))}
+          </div>
+        )}
+        <select
+          className="search-link-type"
+          onChange={(e) => {
+            setLinkType(e.target.value as LinkType);
+          }}
+        >
+          {Object.values(LinkType).map((linkType) => (
+            <option key={linkType} value={linkType}>
+              {capitalizeFirstLetter(linkType)}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="search-bar-row">
+        <button className="add-link-button" onClick={handleAddLink}>
+          Add Link
+        </button>
+      </div>
     </div>
   );
 }
