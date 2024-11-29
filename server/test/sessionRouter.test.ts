ca945c1077a16ca8860c52bc73f00a61094bdae2
junaidshah2940 <shahjunaid1994@gmail.com>
2024-11-29 13:58:15 +0330

    updated tests on sessionRouter

--- server/test/sessionRouter.test.ts
+++ server/test/sessionRouter.test.ts
@@ -1,128 +1,132 @@
+import * as dotenv from "dotenv";
+import { StatusCodes } from "http-status-codes";
+import passport from "passport";
 import request from "supertest";
 import app from "../src/app";
-import passport from "passport";
-import { StatusCodes } from "http-status-codes";
-import dotenv from "dotenv";
+import { User } from "../src/model/user";
+
 dotenv.config();
 
-jest.mock("passport", () => ({
-  authenticate: jest.fn(),
-}));
+jest.mock("../src/model/user", () => {
+  const UserRole = {
+    Developer: "Developer",
+    Resident: "Resident",
+    UrbanPlanner: "UrbanPlanner",
+  };
+  return {
+    User: {
+      login: jest.fn(),
+      getByEmail: jest.fn(),
+      Role: UserRole,
+    },
+    UserRole,
+  };
+});
+
+let plannerCookie: string;
 
 describe("sessionRouter", () => {
-  describe("POST /", () => {
-    it("should login a user successfully", async () => {
-      const mockUser = {
-        id: 1,
-        username: process.env.TEST_NAME,
-        email: process.env.TEST_EMAIL,
-      };
-      passport.authenticate.mockImplementationOnce((strategy, callback) =>
-        callback(null, mockUser),
-      );
+  const mockUser = {
+    email: process.env.SESSION_EMAIL,
+    role: process.env.TEST_ROLE,
+  };
 
-      const response = await request(app)
-        .post("http://localhost:3000/sessions/")
-        .send({
-          username: process.env.TEST_NAME,
-          password: process.env.TEST_PASSWORD,
-        });
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("POST /sessions", () => {
+    it("should successfully authenticate and log in a user", async () => {
+      (User.login as jest.Mock).mockResolvedValue(mockUser);
+
+      const body = {
+        email: process.env.SESSION_EMAIL,
+        password: process.env.SESSION_PASSWORD,
+      };
+
+      const response = await request(app).post("/sessions").send(body);
+
+      plannerCookie = response.headers["set-cookie"];
 
       expect(response.status).toBe(StatusCodes.CREATED);
       expect(response.body).toEqual(mockUser);
     });
 
-    it("should return unauthorized if authentication fails", async () => {
-      passport.authenticate.mockImplementationOnce((strategy, callback) =>
-        callback(null, false, { message: "Invalid credentials" }),
-      );
+    it("should return UNAUTHORIZED if authentication fails", async () => {
+      (User.login as jest.Mock).mockResolvedValue(null);
 
-      const response = await request(app)
-        .post("http://localhost:3000/sessions/")
-        .send({
-          username: process.env.TEST_NAME,
-          password: process.env.TEST_WRONG_PASSWORD,
-        });
+      const body = {
+        email: process.env.SESSION_EMAIL,
+        password: process.env.SESSION_PASSWORD + "wrong",
+      };
+
+      const response = await request(app).post("/sessions").send(body);
 
       expect(response.status).toBe(StatusCodes.UNAUTHORIZED);
-      expect(response.body.message).toBe("Invalid credentials");
-    });
-
-    it("should handle server errors", async () => {
-      passport.authenticate.mockImplementationOnce((strategy, callback) =>
-        callback(new Error("Server error"), false),
-      );
-
-      const response = await request(app)
-        .post("http://localhost:3000/sessions/")
-        .send({
-          username: process.env.TEST_NAME,
-          password: process.env.TEST_PASSWORD,
-        });
-
-      expect(response.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
-      expect(response.body.message).toBe(
-        "An unexpected error occurred on the server",
-      );
+      expect(response.body.message).toBe("Username and/or wrong password");
     });
   });
 
-  describe("GET /current", () => {
-    it("should return the current user if authenticated", async () => {
-      const mockUser = {
-        id: 1,
-        username: process.env.TEST_NAME,
-        email: process.env.TEST_EMAIL,
-      };
+  describe("GET /sessions/current", () => {
+    it("should return the current logged-in user", async () => {
+      (User.getByEmail as jest.Mock).mockResolvedValue(mockUser);
+
       const response = await request(app)
-        .get("http://localhost:3000/sessions/current")
-        .set("Cookie", "connect.sid=s%3A_SESSION_ID")
-        .send();
+        .get("/sessions/current")
+        .set("Cookie", plannerCookie);
 
       expect(response.status).toBe(StatusCodes.OK);
       expect(response.body).toEqual(mockUser);
     });
 
-    it("should return unauthorized if not authenticated", async () => {
-      const response = await request(app)
-        .get("http://localhost:3000/sessions/current")
-        .send();
+    it("should return UNAUTHORIZED if user is not logged in", async () => {
+      const response = await request(app).get("/sessions/current");
 
       expect(response.status).toBe(StatusCodes.UNAUTHORIZED);
-      expect(response.body.error).toBe("Not authenticated");
-    });
-  });
-
-  describe("DELETE /current", () => {
-    it("should log out the current user", async () => {
-      passport.logout = jest
-        .fn()
-        .mockImplementationOnce((callback) => callback());
-
-      const response = await request(app)
-        .delete("http://localhost:3000/sessions/current")
-        .set("Cookie", "connect.sid=s%3A_SESSION_ID")
-        .send();
-
-      expect(response.status).toBe(StatusCodes.NO_CONTENT);
+      expect(response.body.error).toBe("User is not logged in.");
     });
 
-    it("should handle server errors during logout", async () => {
-      passport.logout = jest
-        .fn()
-        .mockImplementationOnce((callback) =>
-          callback(new Error("Logout error")),
-        );
+    it("should handle server errors during session retrieval", async () => {
+      (User.getByEmail as jest.Mock).mockRejectedValue(
+        new Error("Database error"),
+      );
 
       const response = await request(app)
-        .delete("http://localhost:3000/sessions/current")
-        .set("Cookie", "connect.sid=s%3A_SESSION_ID")
-        .send();
+        .get("/sessions/current")
+        .set("Cookie", plannerCookie);
 
       expect(response.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
       expect(response.body.message).toBe(
         "An unexpected error occurred on the server",
       );
+    });
+  });
+
+  describe("DELETE /sessions/current", () => {
+    it("should return UNAUTHORIZED if user is not logged in", async () => {
+      const response = await request(app).delete("/sessions/current");
+
+      expect(response.status).toBe(StatusCodes.UNAUTHORIZED);
+      expect(response.body.error).toBe("User is not logged in.");
+    });
+
+    it("should handle server errors during logout", async () => {
+      const logoutMock = jest
+        .spyOn(passport, "authenticate")
+        .mockImplementation(() => {
+          throw new Error("Logout failed");
+        });
+
+      const response = await request(app)
+        .delete("/sessions/current")
+        .set("Cookie", plannerCookie);
+
+      expect(response.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
+      expect(response.body.message).toBe(
+        "An unexpected error occurred on the server",
+      );
+
+      logoutMock.mockRestore();
     });
   });
 });
