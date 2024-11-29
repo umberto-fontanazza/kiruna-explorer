011dc8602a0d4dfd149fa9971944c63f36843709
SOFIA GARCIA s321387 <s321387@studenti.polito.it>
2024-11-29 08:07:18 +0100

    Checked E2E tests

--- server/test/E2E_document.test.ts
+++ server/test/E2E_document.test.ts
@@ -181,7 +181,7 @@ describe("Testing story 1", () => {
         title: "New title",
         description: "This one will be tested for all fields",
         type: DocumentType.Design,
-        scale: { type: ScaleType.BlueprintsOrEffect },
+        scale: { type: ScaleType.Text },
         stakeholders: [Stakeholder.Lkab],
         issuanceDate: "2021-12-30",
         coordinates: { latitude: 20, longitude: 40 },
@@ -197,7 +197,7 @@ describe("Testing story 1", () => {
         title: "New title",
         description: "This one will be tested for all fields",
         type: DocumentType.Design,
-        scale: { type: ScaleType.Ratio, ratio: 1 },
+        scale: { type: ScaleType.Text },
       });
     expect(response.status).toStrictEqual(StatusCodes.NO_CONTENT);
   });
