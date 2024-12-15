5f2b98129551ac233ffee632461331e287b989ed
SOFIA GARCIA s321387 <s321387@studenti.polito.it>
2024-12-15 20:16:30 +0100

    Remove end-to-end tests for stories 10 and 14 from diagram, waiting to test something

--- server/test/e2e_US10&14_diagram.test.ts
+++
@@ -1,2 +0,0 @@
-// End to end test for story 10: As an Urban Planner, i want to visualize all documents on the diagram
-// End to end test for story 14: As an resident/visitor/urban planner, i want to click on the documents shown on the diagram
