b57e7e87bc913a4a73489139888151b4cb89021c
emanuelefrisi <s1092866@studenti.univpm.it>
2024-12-16 17:27:41 +0100

    Swap coordinates and area of a document managed

--- server/src/model/document.ts
+++ server/src/model/document.ts
@@ -74,7 +74,7 @@ export class Document {
   type: DocumentType;
   scale: Scale;
   stakeholders?: Stakeholder[];
-  coordinates?: Coordinates;
+  private _coordinates?: Coordinates;
   private _area?: Area;
   issuanceTime?: TimeInterval;
   links?: LinkResponseBody[];
@@ -97,7 +97,8 @@ export class Document {
     this.type = type;
     this.scale = scale;
     this.stakeholders = stakeholders;
-    this.coordinates = coordinates;
+    //FIXME:
+    this._coordinates = coordinates;
     if (area) this.setArea(area);
     this.issuanceTime = issuanceTime;
     this.links = links;
@@ -153,11 +154,21 @@ export class Document {
   get area(): Area | undefined {
     return this._area;
   }
-  async setArea(area: Area): Promise<void> {
+  async setArea(area?: Area): Promise<void> {
+    if (this._coordinates && area) await this.setCoordinates(undefined);
     if (this._area) {
       await this._area.delete();
     }
     this._area = area;
+  }
+
+  get coordinates(): Coordinates | undefined {
+    return this._coordinates;
+  }
+
+  async setCoordinates(coordinates?: Coordinates): Promise<void> {
+    if (coordinates && this._area) await this.setArea(undefined);
+    this._coordinates = coordinates;
   }
 
   async update(): Promise<void> {
@@ -279,7 +290,9 @@ export class Document {
     return {
       ...this,
       area: this.area?.toResponseBody(),
-      _area: undefined, //TODO: ho bisogno di un po' di refactoring
+      _area: undefined,
+      coordinates: this._coordinates,
+      _coordinates: undefined,
       issuanceTime: this.issuanceTime ? this.issuanceTime.format() : undefined,
       stakeholders:
         this.stakeholders?.length === 0 ? undefined : this.stakeholders,
