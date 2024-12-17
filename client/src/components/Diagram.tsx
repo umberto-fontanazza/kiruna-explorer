e397cf050e5470d931ebbd32fd217947d127f3be
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-12-17 12:32:45 +0100

    Add icons

--- client/src/components/Diagram.tsx
+++ client/src/components/Diagram.tsx
@@ -32,44 +32,50 @@ const Diagram: FC<DiagramProps> = ({ documents, onDocumentClick }) => {
 
   return (
     <section id="diagram">
-      <div className="scale-types-container">
-        {[...new Set((documents ?? []).map((d) => d.scale.type))]
-          .sort(enumDefOrderComparator(ScaleType))
-          .map((scaleTName) => (
-            <span className={`scale-type ${scaleTName}`} key={scaleTName}>
-              {capitalizeFirstLetter(scaleTName).replace("_", " ")}
-            </span>
-          ))}
+      <div className="time-axis">
+        <div className="combine-axes"></div>
+        <div className="time-cells-container"></div>
       </div>
-      <svg ref={svgRef}>
-        <defs>
-          {Object.values(DocumentType)
-            .map((v) => v.replace("_", "-"))
-            .map((v) => (
-              <pattern
-                id={v}
-                key={v}
-                x="0%"
-                y="0%"
-                height="100%"
-                width="100%"
-                viewBox="0 0 128 128"
-              >
-                <image
+      <div className="scale-n-svg">
+        <div className="scale-types-container">
+          {[...new Set((documents ?? []).map((d) => d.scale.type))]
+            .sort(enumDefOrderComparator(ScaleType))
+            .map((scaleTName) => (
+              <span className={`scale-type ${scaleTName}`} key={scaleTName}>
+                {capitalizeFirstLetter(scaleTName).replace("_", " ")}
+              </span>
+            ))}
+        </div>
+        <svg ref={svgRef}>
+          <defs>
+            {Object.values(DocumentType)
+              .map((v) => v.replace("_", "-"))
+              .map((v) => (
+                <pattern
+                  id={v}
+                  key={v}
                   x="0%"
                   y="0%"
-                  width="128"
-                  height="128"
-                  xlinkHref="/document-material_effect-icon.png" //TODO: parametrize
-                ></image>
-              </pattern>
-            ))}
-        </defs>
-        <g className="links"></g>
-        <g className="documents">
-          <circle r="30" cx="100" cy="100" fill="url(#image)"></circle>
-        </g>
-      </svg>
+                  height="100%"
+                  width="100%"
+                  viewBox="0 0 128 128"
+                >
+                  <image
+                    x="0%"
+                    y="0%"
+                    width="128"
+                    height="128"
+                    xlinkHref="/document-material_effect-icon.png" //TODO: parametrize
+                  ></image>
+                </pattern>
+              ))}
+          </defs>
+          <g className="links"></g>
+          <g className="documents">
+            <circle r="30" cx="100" cy="100" fill="url(#image)"></circle>
+          </g>
+        </svg>
+      </div>
     </section>
   );
 };
